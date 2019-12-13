f36908980845b7a3e796edcf982ec13ff6656b24
Justintime50 <justinpaulhammond@gmail.com>
2019-12-13 10:16:52 -0700

    Fixed linting errors

--- diff.py
+++ diff.py
@@ -1,7 +1,9 @@
+""" Display a diff between two files in HTML. """
+
 import difflib
-from IPython.display import display, HTML
-hdf = difflib.HtmlDiff()
+# from IPython.display import display, HTML # To be used with Jupiter
+HDF = difflib.HtmlDiff()
 F1 = open('f1.html').readlines()
 F2 = open('f2.html').readlines()
-# display(HTML(hdf.make_file(F1, F2))) # To be used with Jupiter
-print(hdf.make_file(F1, F2, context=True, numlines=3))
+# display(HTML(HDF.make_file(F1, F2))) # To be used with Jupiter
+print(HDF.make_file(F1, F2, context=True, numlines=3))
